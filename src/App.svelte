897e1da4666ff99b57a4470b094eb0aab59c094e
Luis Eduardo Gonzalez <luego1986@gmail.com>
2020-05-23 13:32:06 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import "bulma/css/bulma.min.css";
+</script>
+
+<style>
+
+</style>
+
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img
+        src="https://bulma.io/images/bulma-logo-white.png"
+        alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+        width="112"
+        height="28" />
+    </a>
+
+    <a
+      href="javascript:;"
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false">
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </a>
+  </div>
+</nav>
+
+<section class="section">
+  <div class="container">
+    <h1 class="title">Hello World</h1>
+    <p class="subtitle">
+      My first website with
+      <strong>Bulma</strong>
+      !
+    </p>
+  </div>
+</section>
